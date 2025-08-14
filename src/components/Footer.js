fb1bd830dcf4a9567e3e203ee5ad2003da060417
ger-i <geri3dvfx@gmail.com>
2025-08-14 05:56:51 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,8 +25,19 @@ const Footer = () => {
           </ul>
         </div>
 
-        {/* Jobb oldal: üres vagy jövőbeli tartalom */}
-        <div className="sm:w-1/3"></div>
+        {/* Jobb oldal: közösségi ikonok */}
+        <div className="w-full sm:w-1/3 flex justify-center sm:justify-end gap-4 mt-4 sm:mt-0">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+            <svg className="w-6 h-6 text-white hover:text-blue-400 transition" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M22 12a10 10 0 1 0-11.5 9.9v-7h-2v-3h2v-2.3c0-2 1.2-3.1 3-3.1.9 0 1.8.1 1.8.1v2h-1c-1 0-1.3.6-1.3 1.2V12h2.3l-.4 3h-1.9v7A10 10 0 0 0 22 12z" />
+            </svg>
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+            <svg className="w-6 h-6 text-white hover:text-pink-400 transition" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M7 2C4.2 2 2 4.2 2 7v10c0 2.8 2.2 5 5 5h10c2.8 0 5-2.2 5-5V7c0-2.8-2.2-5-5-5H7zm10 2c1.7 0 3 1.3 3 3v10c0 1.7-1.3 3-3 3H7c-1.7 0-3-1.3-3-3V7c0-1.7 1.3-3 3-3h10zm-5 3a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm0 2a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm4.5-2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+            </svg>
+          </a>
+        </div>
       </div>
 
       {/* Alsó jogi szöveg */}
